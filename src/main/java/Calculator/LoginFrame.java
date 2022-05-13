e3ad1dee323238cf3e7508c24d7fe768a9fc976c
akydyrgychov <akydyrgychov@gmail.com>
2022-05-14 04:54:10 +0500

    Добавил наследование JFrame для класса LoginFrame

--- src/main/java/Calculator/LoginFrame.java
+++ src/main/java/Calculator/LoginFrame.java
@@ -1,5 +1,22 @@
 package Calculator;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginFrame {
+//Creating LoginFrame class
+public class LoginFrame extends JFrame implements ActionListener {
 
+
+//Creating constructor of LoginFrame() class
+LoginFrame()
+{
+
+}
+
+//Overriding actionPerformed() method
+@Override
+public void actionPerformed(ActionEvent e) {
+
+}
 }
