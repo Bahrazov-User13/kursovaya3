cad8379d6fe86d68490ce810e7bea030b3dfab80
Yulmukhametov <almaz.yulmukhametov@gmail.com>
2022-05-14 05:03:39 +0500

    Настроил окно авторизации

--- src/main/java/Calculator/Login.java
+++ src/main/java/Calculator/Login.java
@@ -1,7 +1,15 @@
 package Calculator;
 
-public class Login {
-	LoginFrame frame = new LoginFrame();
-	
+import javax.swing.JFrame;
 
+public class Login {
+	public Login () {
+		//Creating object of LoginFrame class and setting some of its properties
+		LoginFrame frame=new LoginFrame();
+		frame.setTitle("Авторизация");
+		frame.setBounds(10,10,370,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
 }
