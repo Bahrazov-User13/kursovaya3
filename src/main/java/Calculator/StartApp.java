ec76b16ac9dafd44d87c0c4bc82869c2536c0785
Bahrazov-User13 <bahrazov@gmail.com>
2022-05-14 06:37:26 +0500

    Исправил недороботки

--- src/main/java/Calculator/StartApp.java
+++ src/main/java/Calculator/StartApp.java
@@ -2,15 +2,15 @@ package Calculator;
 
 // TODO: Auto-generated Javadoc
 /**
- * The Class StartApp.
- * @author Bakhrazov R.R.
- */
+* The Class StartApp.
+* @author Bakhrazov R.R.
+*/
 public class StartApp {
-    
-    /**
-     * The main method.
-     *
-     * @param args the arguments
-     */
-    public static void main(String[] args) { new FormMain("Депозитный калькулятор с капитализацией"); }
+
+/**
+* The main method.
+*
+* @param args the arguments
+*/
+public static void main(String[] args) { new Login (); }
 }
